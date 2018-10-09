fc1486bef7fe9d9033e7602d0283dfaee9d2afb6
zs1843 <zhandrea1843@qq.com>
2018-10-09 14:40:01 +0800

    'add'

--- miniprogram/pages/2048/2048.js
+++ miniprogram/pages/2048/2048.js
@@ -6,5 +6,38 @@ Page({
     this.setData({
       hidden: true,
     })
+  },
+  moveDown: function(){
+
+  },
+  moveUp: function () {
+
+  },
+  moveLeft: function () {
+
+  },
+  moveRight: function () {
+
+  },
+  canMoveDown: function(){
+
+  },
+  canMoveUp: function () {
+
+  },
+  canMoveLeft: function () {
+
+  },
+  canMoveRight: function () {
+
+  },
+  updateView: function(){
+
+  },
+  gameOver: function(){
+
+  },
+  isGameOver: function(){
+
   }
 })
